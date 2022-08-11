6fb060739892d9f42411f01a11c7337d0286921c
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2022-08-11 10:51:19 +0200

    updated chinaroots.R to have no row names

--- _data/R/chinaroots.R
+++ _data/R/chinaroots.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(tidyverse)
 library(R.utils)
 
+root.dir = getwd()
+
 # create output directory
 if(!dir.exists("_data/R/summaries")){dir.create("_data/R/summaries")}  
 if(!dir.exists("_data/R/temp")){dir.create("_data/R/temp")}
@@ -53,7 +55,9 @@ traits_summary$accessDate <- Sys.Date()
 
 head(traits_summary)
 
-write.csv(traits_summary, file=paste("../summaries/",dataset,".csv",sep="") )
-gzip(paste("../summaries/",dataset,".csv",sep=""), destname=paste("../summaries/",dataset,".csv.gz",sep=""))
+
+write.csv(traits_summary, file=paste("../summaries/",dataset,".csv",sep=""), row.names = F )
+gzip(paste("../summaries/",dataset,".csv",sep=""), destname=paste("../summaries/",dataset,".csv.gz",sep=""),overwrite=T)
 unlink(paste("../summaries/",dataset,".csv",sep=""))
 
+setwd(root.dir)
