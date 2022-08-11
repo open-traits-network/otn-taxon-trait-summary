d7358cb9b24fb0bbc70cddefa79397c436d4c0d4
jhpoelen <jhpoelen@xs4all.nl>
2022-08-11 12:13:10 +0200

    update alignment script

--- _data/align-names.sh
+++ _data/align-names.sh
@@ -2,6 +2,13 @@
 #
 # track registered OTN datasets and generate dataset summaries
 #
+# uses:
+# 	https://github.com/globalbioticinteractions/nomer for name alignment with catalogue of life
+#   miller (mlr) for table manipulation
+#
+# author: https://opentraits.org/members/jorrit-h-poelen 
+# date: 2022-08-11
+# 
 
 set -xe
 
@@ -15,21 +22,21 @@ function align-names {
 ls -1 R/summaries/*.csv.gz\
  | xargs -L1 -I {} sh -c 'cat {} | gunzip | mlr --icsv cat'\
  | gzip\
- > R/summaries/agg.dkvp.gz
+ > R/_all.dkvp.gz
 
-cat R/summaries/agg.dkvp.gz\
+cat R/_all.dkvp.gz\
  | gunzip\
  | mlr --ocsv unsparsify\
  | mlr --csv reorder -f "$(head -n1 template-summary-table.csv)"\
  | mlr --icsv --otsv cat\
  | gzip\
- > R/summaries/agg.tsv.gz
+ > R/_agg.tsv.gz
  
-cat R/summaries/agg.tsv.gz\
+cat R/_agg.tsv.gz\
  | gunzip\
  | tail -n+2\
  | align-names\
  | gzip\
- > R/summaries/agg-aligned.tsv.gz
+ > R/_all-aligned.tsv.gz
 
 
